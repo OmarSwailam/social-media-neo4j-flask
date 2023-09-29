66186a36d9157c37b72ef21b5424cfb696465891
Omar Swailam <omarswailam@outlook.com>
2023-09-30 01:59:33 +0300

    create edit_post route

--- app/models/post.py
+++ app/models/post.py
@@ -1,4 +1,4 @@
-from py2neo import Node, Relationship
+from py2neo import Node, Relationship, NodeMatcher
 from app import graph
 
 
@@ -15,3 +15,34 @@ class Post:
             graph.create(post)
             relationship = Relationship(user_node, "POSTED", post)
             graph.create(relationship)
+
+    def edit(self, new_text, new_images):
+        if new_text:
+            self.text = new_text
+        if new_images:
+            self.images = new_images
+
+        post_node = graph.nodes.match("Post", uuid=self.uuid).first()
+        if post_node:
+            post_node["text"] = self.text
+            post_node["images"] = self.images
+            post_node.push()
+
+    @classmethod
+    def find_by_id(cls, post_uuid):
+        matcher = NodeMatcher(graph)
+        post_node = matcher.match("Post").where(uuid=post_uuid).first()
+
+        if post_node:
+            user_relationship = graph.match((None, post_node), r_type="POSTED").first()
+
+            if user_relationship:
+                user_node = user_relationship.nodes()[0]
+                return {
+                    "uuid": post_node["uuid"],
+                    "user_uuid": user_node["uuid"],
+                    "text": post_node["text"],
+                    "images": post_node["images"],
+                }
+
+        return None
--- app/routes/post_routes.py
+++ app/routes/post_routes.py
@@ -21,3 +21,28 @@ def create_post():
     new_post.create()
 
     return jsonify({"message": "Post created successfully"}), 201
+
+
+@posts_bp.route("/post/<post_uuid>", methods=["PUT"])
+@jwt_required()
+def edit_post(post_uuid):
+    post = Post.find_by_id(post_uuid)
+
+    if not post:
+        return jsonify({"error": "Post not found"}), 404
+
+    current_user_identity = get_jwt_identity()
+
+    if current_user_identity != post["user_uuid"]:
+        return jsonify({"error": "You can only edit your own posts"}), 403
+
+    data = request.get_json()
+    new_text = data.get("text", "")
+    new_images = data.get("images", [])
+
+    if not new_text and not new_images:
+        return jsonify({"error": "A post must have text and/or images"}), 400
+
+    post.edit(new_text, new_images)
+
+    return jsonify({"message": "Post edited successfully"}), 200
