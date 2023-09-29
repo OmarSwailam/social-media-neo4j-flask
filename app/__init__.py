cdf9bc324c6d53fded501cc028afdd7a96b0de51
Omar Swailam <omarswailam@outlook.com>
2023-09-30 00:39:52 +0300

    create flask app

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from py2neo import Graph
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+jwt = JWTManager(app)
+
+graph = Graph("bolt://localhost:7687", auth=("neo4j", "password"))
+
+from app.routes import user_routes
