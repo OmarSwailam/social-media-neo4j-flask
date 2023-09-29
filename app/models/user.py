0e3cdd5159ad9a0fbe91b9773d52d9e97ecca44e
Omar Swailam <omarswailam@outlook.com>
2023-09-30 00:47:11 +0300

    create user model

---
+++ app/models/user.py
@@ -0,0 +1,21 @@
+from py2neo import Node, NodeMatcher
+from passlib.hash import pbkdf2_sha256
+from app import graph
+
+class User:
+    def __init__(self, first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    def create(self):
+        hashed_password = pbkdf2_sha256.hash(self.password)
+        user = Node("User", first_name=self.first_name, last_name=self.last_name, email=self.email, password=hashed_password)
+        graph.create(user)
+
+    @classmethod
+    def find_by_email(cls, email):
+        matcher = NodeMatcher(graph)
+        user = matcher.match("User").where(email=email).first()
+        return user
