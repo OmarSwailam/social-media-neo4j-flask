cf7e1bfe2b4d02b09efa1ccf3b69ab56c0a4fe68
Omar Swailam <omarswailam@outlook.com>
2023-10-02 11:04:51 +0300

    refactor: use classbasedviews "Method view""

--- app/routes/post_routes.py
+++ app/routes/post_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+from flask.views import MethodView
 from app.models.user import User
 from app.models.post import Post
 from flask_jwt_extended import jwt_required, get_jwt_identity
@@ -6,77 +7,94 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 posts_bp = Blueprint("posts_bp", __name__)
 
 
-@posts_bp.route("/posts", methods=["GET"])
-def get_all_posts():
-    posts = Post.get_all_posts()
+class PostAPI(MethodView):
+    @jwt_required()
+    def get(self, post_uuid=None):
+        if post_uuid:
+            post = Post.find_by_id(post_uuid)
+            if not post:
+                return jsonify({"error": "Post not found"}), 404
+            return (
+                jsonify(
+                    {
+                        "uuid": post["uuid"],
+                        "user_uuid": post["user_uuid"],
+                        "text": post["text"],
+                        "images": post["images"],
+                    }
+                ),
+                200,
+            )
+        else:
+            posts = Post.get_all_posts()
+            post_list = []
+            for post in posts:
+                post_data = {
+                    "uuid": post["uuid"],
+                    "user_uuid": post["user_uuid"],
+                    "text": post["text"],
+                    "images": post["images"],
+                }
+                post_list.append(post_data)
 
-    post_list = []
-    for post in posts:
-        post_data = {
-            "uuid": post["uuid"],
-            "user_uuid": post["user_uuid"],
-            "text": post["text"],
-            "images": post["images"],
-        }
-        post_list.append(post_data)
+            return jsonify(post_list), 200
 
-    return jsonify(post_list), 200
+    @jwt_required()
+    def post(self):
+        current_user_uuid = get_jwt_identity()
+        data = request.get_json()
+        text = data.get("text", "")
+        images = data.get("images", [])
+
+        if not text and not images:
+            return jsonify({"error": "A post must have text and/or images"}), 400
+
+        new_post = Post(current_user_uuid, text, images)
+        new_post.create()
+
+        return jsonify({"message": "Post created successfully"}), 201
+
+    @jwt_required()
+    def put(self, post_uuid):
+        post = Post.find_by_id(post_uuid)
+
+        if not post:
+            return jsonify({"error": "Post not found"}), 404
+
+        current_user_identity = get_jwt_identity()
+
+        if current_user_identity != post["user_uuid"]:
+            return jsonify({"error": "You can only edit your own posts"}), 403
+
+        data = request.get_json()
+        new_text = data.get("text", "")
+        new_images = data.get("images", [])
+
+        if not new_text and not new_images:
+            return jsonify({"error": "A post must have text and/or images"}), 400
+
+        post.edit(new_text, new_images)
+
+        return jsonify({"message": "Post edited successfully"}), 200
+
+    @jwt_required()
+    def delete(self, post_uuid):
+        current_user_identity = get_jwt_identity()
+        post = Post.find_by_id(post_uuid)
+
+        if not post:
+            return jsonify({"error": "Post not found"}), 404
+
+        if current_user_identity != post["user_uuid"]:
+            return jsonify({"error": "You can only delete your own posts"}), 403
+
+        post.delete()
+
+        return jsonify({"message": "Post deleted successfully"}), 200
 
 
-@posts_bp.route("/posts", methods=["POST"])
-@jwt_required()
-def create_post():
-    current_user_uuid = get_jwt_identity()
-    data = request.get_json()
-    text = data.get("text", "")
-    images = data.get("images", [])
-
-    if not text and not images:
-        return jsonify({"error": "A post must have text and/or images"}), 400
-
-    new_post = Post(current_user_uuid, text, images)
-    new_post.create()
-
-    return jsonify({"message": "Post created successfully"}), 201
-
-
-@posts_bp.route("/post/<post_uuid>", methods=["PUT"])
-@jwt_required()
-def edit_post(post_uuid):
-    post = Post.find_by_id(post_uuid)
-
-    if not post:
-        return jsonify({"error": "Post not found"}), 404
-
-    current_user_identity = get_jwt_identity()
-
-    if current_user_identity != post["user_uuid"]:
-        return jsonify({"error": "You can only edit your own posts"}), 403
-
-    data = request.get_json()
-    new_text = data.get("text", "")
-    new_images = data.get("images", [])
-
-    if not new_text and not new_images:
-        return jsonify({"error": "A post must have text and/or images"}), 400
-
-    post.edit(new_text, new_images)
-
-    return jsonify({"message": "Post edited successfully"}), 200
-
-
-@posts_bp.route("/posts/<post_uuid>", methods=["DELETE"])
-@jwt_required()
-def delete_post(post_uuid):
-    current_user_identity = get_jwt_identity()
-    post = Post.find_by_id(post_uuid)
-
-    if not post:
-        return jsonify({"error": "Post not found"}), 404
-
-    if current_user_identity != post["user_uuid"]:
-        return jsonify({"error": "You can only delete your own posts"}), 403
-
-    post.delete()
-
-    return jsonify({"message": "Post deleted successfully"}), 200
+post_view = PostAPI.as_view("post_api")
+posts_bp.add_url_rule("/posts", view_func=post_view, methods=["GET", "POST"])
+posts_bp.add_url_rule(
+    "/post/<post_uuid>", view_func=post_view, methods=["GET", "PUT", "DELETE"]
+)
--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+from flask.views import MethodView
 from app.models.user import User
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from passlib.hash import pbkdf2_sha256
@@ -7,152 +8,139 @@ from app.models.utils.follow_manager import FollowManager
 user_bp = Blueprint("user_bp", __name__)
 
 
-@user_bp.route("/register", methods=["POST"])
-def register():
-    data = request.get_json()
-    first_name = data.get("first_name")
-    last_name = data.get("last_name")
-    email = data.get("email")
-    password = data.get("password")
+class UserRegistration(MethodView):
+    def post(self):
+        data = request.get_json()
+        first_name = data.get("first_name")
+        last_name = data.get("last_name")
+        email = data.get("email")
+        password = data.get("password")
 
-    if not first_name or not last_name or not email or not password:
-        return jsonify({"error": "All fields are required"}), 400
+        if not first_name or not last_name or not email or not password:
+            return jsonify({"error": "All fields are required"}), 400
 
-    existing_user = User.find_by_email(email)
-    if existing_user:
-        return jsonify({"error": "Email is already in use"}), 400
+        existing_user = User.find_by_email(email)
+        if existing_user:
+            return jsonify({"error": "Email is already in use"}), 400
 
-    new_user = User(first_name, last_name, email, password)
-    new_user.create()
+        new_user = User(first_name, last_name, email, password)
+        new_user.create()
 
-    token = create_access_token(identity=email)
+        token = create_access_token(identity=email)
 
-    return jsonify({"message": "User registered successfully", "token": token}), 201
-
-
-@user_bp.route("/login", methods=["POST"])
-def login():
-    data = request.get_json()
-    email = data.get("email")
-    password = data.get("password")
-
-    if not email or not password:
-        return jsonify({"error": "Email and password are required"}), 400
-
-    user = User.find_by_email(email)
-
-    if not user or not pbkdf2_sha256.verify(password, user["password"]):
-        return jsonify({"error": "Invalid credentials"}), 401
-
-    token = create_access_token(identity=email)
-
-    return jsonify({"token": token}), 200
+        return jsonify({"message": "User registered successfully", "token": token}), 201
 
 
-@user_bp.route("/profile", methods=["GET"])
-@jwt_required()
-def profile():
-    current_user_identity = get_jwt_identity()
-    if not current_user_identity:
-        return jsonify({"error": "Authentication required"}), 401
+class UserLogin(MethodView):
+    def post(self):
+        data = request.get_json()
+        email = data.get("email")
+        password = data.get("password")
 
-    user = User.find_by_email(current_user_identity)
-    if not user:
-        return jsonify({"error": "User not found"}), 404
+        if not email or not password:
+            return jsonify({"error": "Email and password are required"}), 400
 
-    followers_count = FollowManager.get_followers_count(user.uuid)
-    following_count = FollowManager.get_following_count(user.uuid)
+        user = User.find_by_email(email)
 
-    user_data = {
-        "first_name": user["first_name"],
-        "last_name": user["last_name"],
-        "email": user["email"],
-        "followers_count": followers_count,
-        "following_count": following_count,
-    }
+        if not user or not pbkdf2_sha256.verify(password, user["password"]):
+            return jsonify({"error": "Invalid credentials"}), 401
 
-    return jsonify(user_data), 200
+        token = create_access_token(identity=email)
+
+        return jsonify({"token": token}), 200
 
 
-@user_bp.route("/users", methods=["GET"])
-@jwt_required()
-def get_all_users():
-    users = User.get_all_users()
-
-    user_list = []
-    for user in users:
-        user_data = {
-            "uuid": user["uuid"],
-            "first_name": user["first_name"],
-            "last_name": user["last_name"],
-            "email": user["email"],
-        }
-        user_list.append(user_data)
-
-    return jsonify(user_list), 200
-
-
-@user_bp.route("/user/<uuid>", methods=["GET"])
-@jwt_required()
-def get_user_by_uuid(uuid):
-    user = User.find_by_id(uuid)
-
-    if not user:
-        return jsonify({"error": "User not found"}), 404
-
-    followers_count = FollowManager.get_followers_count(user.uuid)
-    following_count = FollowManager.get_following_count(user.uuid)
-    
-    user_data = {
-        "first_name": user["first_name"],
-        "last_name": user["last_name"],
-        "email": user["email"],
-        "followers_count": followers_count,
-        "following_count": following_count,
-    }
-
-    return jsonify(user_data), 200
+class UserAPI(MethodView):
+    @jwt_required()
+    def get(self, uuid=None):
+        if uuid is None:
+            users = User.get_all_users()
+            user_list = []
+            for user in users:
+                user_data = {
+                    "uuid": user["uuid"],
+                    "first_name": user["first_name"],
+                    "last_name": user["last_name"],
+                    "email": user["email"],
+                }
+                user_list.append(user_data)
+            return jsonify(user_list), 200
+        else:
+            user = User.find_by_id(uuid)
+            if not user:
+                return jsonify({"error": "User not found"}), 404
+            followers_count = FollowManager.get_followers_count(user.uuid)
+            following_count = FollowManager.get_following_count(user.uuid)
+            user_data = {
+                "first_name": user["first_name"],
+                "last_name": user["last_name"],
+                "email": user["email"],
+                "followers_count": followers_count,
+                "following_count": following_count,
+            }
+            return jsonify(user_data), 200
 
 
-@user_bp.route("/users/<user_id>/follow", methods=["POST"])
-@jwt_required()
-def follow_user(user_id):
-    current_user_identity = get_jwt_identity()
-    followed_successful = FollowManager.follow_user(current_user_identity, user_id)
-    if followed_successful:
-        return jsonify({"message": "User followed successfully"}), 200
-    else:
-        return jsonify({"error": "User(s) not found"}), 404
+class FollowUserAPI(MethodView):
+    @jwt_required()
+    def post(self, user_id):
+        current_user_identity = get_jwt_identity()
+        followed_successful = FollowManager.follow_user(current_user_identity, user_id)
+        if followed_successful:
+            return jsonify({"message": "User followed successfully"}), 200
+        else:
+            return jsonify({"error": "User(s) not found"}), 404
+
+    @jwt_required()
+    def delete(self, user_id):
+        current_user_identity = get_jwt_identity()
+        unfollowed_successful = FollowManager.unfollow_user(
+            current_user_identity, user_id
+        )
+        if unfollowed_successful:
+            return jsonify({"message": "User unfollowed successfully"}), 200
+        else:
+            return jsonify({"error": "User(s) not found"}), 404
 
 
-@user_bp.route("/users/<user_id>/unfollow", methods=["POST"])
-@jwt_required()
-def unfollow_user(user_id):
-    current_user_identity = get_jwt_identity()
-    unfollowed_successful = FollowManager.unfollow_user(current_user_identity, user_id)
-    if unfollowed_successful:
-        return jsonify({"message": "User unfollowed successfully"}), 200
-    else:
-        return jsonify({"error": "User(s) not found"}), 404
+class FollowAPI(MethodView):
+    @jwt_required()
+    def get(self, user_id, action):
+        if action == "followers":
+            followers = FollowManager.get_followers(user_id)
+            follower_data = [
+                {"uuid": follower["uuid"], "username": follower["username"]}
+                for follower in followers
+            ]
+            return jsonify({"followers": follower_data}), 200
+        elif action == "following":
+            following = FollowManager.get_following(user_id)
+            following_data = [
+                {"uuid": followed["uuid"], "username": followed["username"]}
+                for followed in following
+            ]
+            return jsonify({"following": following_data}), 200
+        else:
+            return jsonify({"error": "Invalid action"}), 400
 
 
-@user_bp.route("/users/<user_id>/followers", methods=["GET"])
-@jwt_required()
-def get_followers(user_id):
-    followers = FollowManager.get_followers(user_id)
-    follower_data = [
-        {"uuid": follower["uuid"], "username": follower["username"]}
-        for follower in followers
-    ]
-    return jsonify({"followers": follower_data}), 200
+user_bp.add_url_rule("/register", view_func=UserRegistration.as_view("register"))
+user_bp.add_url_rule("/login", view_func=UserLogin.as_view("login"))
+
+user_view = UserAPI.as_view("user_api")
+user_bp.add_url_rule("/users", view_func=user_view, methods=["GET"])
+user_bp.add_url_rule("/user/<uuid>", view_func=user_view, methods=["GET"])
 
 
-@user_bp.route("/users/<user_id>/following", methods=["GET"])
-@jwt_required()
-def get_following(user_id):
-    following = FollowManager.get_following(user_id)
-    following_data = [
-        {"uuid": followed["uuid"], "username": followed["username"]}
-        for followed in following
-    ]
-    return jsonify({"following": following_data}), 200
+follow_user_view = FollowUserAPI.as_view("follow_user_api")
+user_bp.add_url_rule(
+    "/users/<user_id>/follow", view_func=follow_user_view, methods=["POST"]
+)
+user_bp.add_url_rule(
+    "/users/<user_id>/unfollow", view_func=follow_user_view, methods=["DELETE"]
+)
+
+follow_view = FollowAPI.as_view("follow_api")
+user_bp.add_url_rule(
+    "/users/<user_id>/<action>", view_func=follow_view, methods=["GET"]
+)
